9b2c57ad8282eca81ed12d70e8924f0d60ab3bfc
Ariel Olguin <arielolg@arielPC.arielpc.net>
2019-03-18 22:57:40 -0300

    probando git desde VS

--- js/vistaCompleja.js
+++ js/vistaCompleja.js
@@ -10,3 +10,5 @@ vistaCompleja.prototype.mostrar = function() {
   console.log('Titulo: ', this.modelo.titulo, ' Autor: ', this.modelo.autor);
   console.log('Resumen: ', this.modelo.resumen, ' Precio: ', this.modelo.precio);
 }
+
+
